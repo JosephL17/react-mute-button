e408beafce646f784d7fea59996d73d5bca818b5
josephl17 <joseph.lauria17@gmail.com>
2024-06-26 15:47:56 -0500

    toggle icon function works

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [onIcon, setOnIcon] = useState(true)
+  const icon = "./icons/on.svg"
+  const offIcon = './icons/off.svg'
+
+  return (
+    <>
+      <h1>Hello</h1>
+      
+      <img src={onIcon ? icon : offIcon} alt="Icon" onClick={() => setOnIcon(!onIcon)} />
+    </>
+  )
+}
+
+export default App
